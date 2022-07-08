9645bb55504480d7a604be505880c0738f3cae3e
blaqcomrade <samgraphics001@gmail.com>
2022-07-08 13:54:10 +0100

    using handling exceptions to handle errors

---
+++ handling-exceptions/handling-exceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    string num1;
+    string num2;
+
+    Console.WriteLine("enter first number");
+    num1 = Console.ReadLine();
+    Console.WriteLine("enter second number");
+    num2 = Console.ReadLine();
+
+    Console.WriteLine("Answer");
+    Console.WriteLine((decimal)int.Parse(num1) / int.Parse(num2));
+}
+catch (FormatException)
+{
+    Console.WriteLine("string not allowed");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("You can't divide by zero");
+}
+catch (Exception error)
+{
+    Console.WriteLine(error);
+}
