4831365d205e7cc944f83c5a0cc07e9dbcf3d6d5
blaqcomrade <samgraphics001@gmail.com>
2022-07-14 14:47:31 +0100

    linq

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Linq
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student("patrick","ogbo","jss3",48.6),
+                new Student("john","eje","ss3",70.0),
+                new Student("isaac","ugbabe","jss1",-41.6),
+                new Student("vincent","otopka","jss1",80.0),
+                new Student("peter","ada","ss3",99.9),
+                new Student("lech","philip","jss2",80.0),
+                new Student("edward","ode","ss3",99.9),
+            };
+
+            //var querysyntax = from obj in students
+            //                  where obj.current_class == "jss1"
+            //                  select obj;
+
+            var methodsyntax = students.Where(obj => obj.current_class == "jss1" && obj.result_average >= 50);
+           
+
+            var mixedsyntax = (from obj in students
+                               select obj.result_average).Max();
+
+            //Console.WriteLine("Max value =" + mixedsyntax);
+
+            foreach (var student in methodsyntax)
+            {
+                Console.WriteLine($"Name:{student.first_name}, surname:{student.surname}, current class:{student.current_class}, Average:{student.result_average}\n");
+            }
+
+        }
+    }
+}
---
+++ Linq/Linq/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Student
+    {
+        public string first_name { get; set; }
+        public string surname { get; set; }
+        public string current_class { get; set; }
+        public double result_average { get; set; }
+
+
+
+        public Student()
+        { }
+
+        public Student(string first, string sur_name, string current_clas, double result_avg)
+        {
+           first_name = first;
+           surname = sur_name;
+           current_class = current_clas;
+           result_average = result_avg;
+        }
+        
+    }
+}
