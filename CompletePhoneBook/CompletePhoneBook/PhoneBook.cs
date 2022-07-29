9ff208495c6b2c69c488ab1f5f6f192c34519f51
blaqcomrade <samgraphics001@gmail.com>
2022-07-29 16:18:14 +0100

    changes

---
+++ CompletePhoneBook/CompletePhoneBook/PhoneBook.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompletePhoneBook
+{
+    internal class PhoneBook
+    {
+       
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+       
+
+        public void Add(string name, string phonenumber)
+        {
+            try
+            {
+                phonebook.Add(name, phonenumber);
+                Console.WriteLine("SUCCESSFUL\n");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("NAME ALREADY EXIST");
+                Console.WriteLine("FAILED\n");
+            }
+           
+            
+        }
+
+        public void View(string name)
+        {
+            if (phonebook.ContainsKey(name))
+            {
+                Console.WriteLine(phonebook[name] + "\n");
+            }
+            else
+            {
+                Console.WriteLine("CONTACT ALREADY EXIST\n");
+            }
+        }
+        
+        public void Update(string name, string phonenumber)
+        {
+            if (phonebook.ContainsKey(name))
+            {
+                phonebook[name] = phonenumber;
+                Console.WriteLine($"Name:{name} successfully updated Number to {phonenumber}\n");
+            }
+            else
+            {
+                Console.WriteLine("CONTACT ALREADY EXIST\n");
+            }
+        }
+
+        public void Delete(string name)
+        {
+            if (phonebook.ContainsKey(name))
+            {
+                phonebook.Remove(name);
+                Console.WriteLine("CONTACT SUCCESSFULLY DELETED\n");
+                
+            }
+            else
+            {
+                Console.WriteLine("CONTACT ALREADY EXIST\n");
+            }
+        }
+    }
+}
---
+++ CompletePhoneBook/CompletePhoneBook/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompletePhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                            WELCOME TO PHONE BOOK APP");
+            PhoneBook contact = new PhoneBook();
+            while (true)
+            {
+               
+
+                string userinput = Console.ReadLine();
+                var spilt = userinput.Split();
+                var opt = spilt[0];
+                
+
+                if (opt == "save")
+                {
+                    var name = spilt[1];
+                    var phonenumber = spilt[2];
+                    contact.Add(name, phonenumber);
+                }
+                else if (opt == "view")
+                {
+                    var name = spilt[1];
+                    contact.View(name);
+                }
+                else if (opt == "delete")
+                {
+                    var name = spilt[1];
+                    var phonenumber = spilt[2];
+                    contact.Delete(name);
+                }
+                else if (opt == "update")
+                {
+                    var name = spilt[1];
+                    var phonenumber = spilt[2];
+                    contact.Update(name , phonenumber);
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+}
+}
+
